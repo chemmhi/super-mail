ec42566c0adbc1646ed9006722b2d57a3b892a0a
chenhai <1051585831@qq.com>
2021-07-21 22:48:52 +0800

    add another method to set the vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,28 @@
 
-module.exports={
-  configureWebpack:{
-    resolve:{
-      alias:{
-        'assets':'@/assets',
-        'components':'@/components',
-        // 'network':path.resolve(src_path,'network'),
-        'network':'@/network',
-        'views':'@/views',
-      }
-    }
+// module.exports={
+//   configureWebpack:{
+//     resolve:{
+//       alias:{
+//         'assets':'@/assets',
+//         'components':'@/components',
+//         // 'network':path.resolve(src_path,'network'),
+//         'network':'@/network',
+//         'views':'@/views',
+//       }
+//     }
+//   }
+// }
+
+const path = require('path')
+module.exports = {
+  publicPath:'/dist/',
+  chainWebpack: config => {
+    config.resolve.alias
+        // .set("@",path.join(__dirname,'src'))
+        .set("components",path.join(__dirname,'src/components'))
+        .set('views',path.join(__dirname,'src/views'))
+        .set('assets',path.join(__dirname,'src/assets'))
+        .set('network',path.join(__dirname,'src/network'))
+
   }
 }
