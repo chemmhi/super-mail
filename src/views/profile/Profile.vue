4934be5c032dfab8c60d0e41119a82527de6ab12
chenhai <1051585831@qq.com>
2021-08-04 23:25:09 +0800

    '完成了Prifile的一部分'

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,23 +1,97 @@
 <template>
-  <grid-view>
-    <div v-for="i in 11">
-      <div>hello world</div>
-    </div>
-  </grid-view>
+  <div id="profile">
+    <nav-bar class="nav-bar"><div slot="center">小码哥商城</div></nav-bar>
+    <UserInfo></UserInfo>
 
+    <section class="account">
+      <div class="account-item">
+        <div class="number">
+          <span class="balance">0.00</span>元
+        </div>
+        <div class="account-info">我的余额</div>
+      </div>
+      <div class="account-item">
+        <div class="number">
+          <span class="balance">0</span>个
+        </div>
+        <div class="account-info">我的优惠</div>
+      </div>
+      <div class="account-item">
+        <div class="number">
+          <span class="balance">0</span>分
+        </div>
+        <div class="account-info">我的积分</div>
+      </div>
+    </section>
+
+    <list-view :list-data="orderList" class="order-list"></list-view>
+    <list-view :list-data="serviceList" class="service-list"></list-view>
+  </div>
 </template>
 
 <script>
-import GridView from "@/components/common/grid-view/GridView";
-
-export default {
-  name: "Profile",
-  components:{
-    GridView,
-  }
-}
+  import UserInfo from "@/views/profile/children-components/UserInfo";
+  // import ListView from './childComps/ListView'
+  import NavBar from "@/components/common/navbar/NavBar";
+	export default {
+		name: "Profile",
+    components: {
+		  NavBar,
+      UserInfo,
+    },
+    data: function () {
+		  return {
+		    orderList: [
+          {icon: '#order', iconColor: '#ff8198', info: '我的消息'},
+          {icon: '#point', iconColor: '#fc7b53', info: '积分商城'},
+          {icon: '#vip', iconColor: '#ffc636', info: '会员卡'},
+        ],
+        serviceList: [
+          {icon: '#service', iconColor: '#ff8198', info: '我的购物车'},
+          {icon: '#download', iconColor: '#ff8198', info: '下载购物APP'},
+        ]
+      }
+    },
+    mounted: function () {
+    }
+	}
 </script>
 
 <style scoped>
-
+  #profile {
+    background-color: #f2f2f2;
+  }
+  .nav-bar {
+    background-color: var(--color-tint);
+    font-weight: 700;
+    color: #fff;
+  }
+  .account {
+    display: flex;
+  }
+  .account-item {
+    width: 100%;
+    background-color: #fff;
+    margin-right: 1px;
+    text-align: center;
+  }
+  .account-item:last-of-type {
+    margin-right: 0;
+  }
+  .account-item {
+    color: #666;
+    font-size: 13px;
+    padding: 18px;
+  }
+  .account-item .balance {
+    font-size: 24px;
+    font-weight: 700;
+    color: #ff5f3e;
+  }
+  .account-info {
+    margin-top: 6px;
+  }
+  .order-list, .service-list {
+    margin-top: 12px;
+  }
 </style>
